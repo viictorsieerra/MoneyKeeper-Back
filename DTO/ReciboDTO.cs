ac88d61f64188f8a212d69c0fdc369500a88e183
CoroAlfonso <licencias@svalero.com>
2025-03-12 18:37:37 +0100

    añadido añadir recibo desde cuenta

--- DTO/ReciboDTO.cs
+++ DTO/ReciboDTO.cs
@@ -7,14 +7,15 @@ public class ReciboDTO
     public decimal _dineroRecibo { get; set; }
     public bool _activa { get; set; }
     public DateTime _fecRecibo { get; set; } = DateTime.Now;
+    public string _nombreRecibo {get; set;}
 
     public ReciboDTO() { }
 
-    public ReciboDTO(decimal dineroRecibido, bool activa, DateTime fecRecibo)
+    public ReciboDTO(decimal dineroRecibido, bool activa, DateTime fecRecibo, string nombreRecibo)
     {
         _dineroRecibo = dineroRecibido;
         _activa = activa;
         _fecRecibo = fecRecibo;
-
+        _nombreRecibo = nombreRecibo;
     }
 }
